bb33b03c876c9c986486075d3ec54acab516bcf4
khoi <khoiracle@gmail.com>
2020-01-04 15:31:06 +0700

    Unknown slowness

--- boring_robot.py
+++ boring_robot.py
@@ -5,8 +5,8 @@ from bd import BlueDot
 
 from libs import BoringTapeSensor
 
-SPEED = 0.15
-TURN_SPEED = 0.13
+SPEED = 0.2
+TURN_SPEED = 0.2
 MANUAL_CONTROL_SPEED = 0.25
 
 
@@ -32,40 +32,38 @@ def manual_control():
 
 def line_following_engine():
     action = (0, 0)
-    prev_state = None
     while True:
         state = (most_left_sensor.line_detected, left_sensor.line_detected,
                  mid_sensor.line_detected, right_sensor.line_detected, most_right_sensor.line_detected)
         distance_in_cm = distance_sensor.distance * 100
-        print(f"state {state} distance {distance_in_cm}")
+        # print(f"state {state} distance {distance_in_cm}")
         if distance_in_cm < 15:
-            print("about to hit something")
+            # print("about to hit something")
             action = (0, 0)
         elif state == (False, False, True, False, False):
-            print("Going straight")
+            # print("Going straight")
             action = (1 * SPEED, 1 * SPEED)
         elif state == (False, False, True, True, False):
-            print("right and mid - turning right")
+            # print("right and mid - turning right")
             action = (1 * TURN_SPEED, 0)
         elif state == (False, False, False, True, False):
-            print("right - turning further right")
+            # print("right - turning further right")
             action = (1 * TURN_SPEED, 0)
         elif state == (False, True, True, False, False):
-            print("left and mid-  turning left")
+            # print("left and mid-  turning left")
             action = (0, 1 * TURN_SPEED)
         elif state == (False, True, False, False, False):
-            print("left  - turning further left")
+            # print("left  - turning further left")
             action = (0, 1 * TURN_SPEED)
         elif state == (False, False, True, True, True):
-            print("right crazy")
+            # print("right crazy")
             action = (1.2 * TURN_SPEED, -1 * TURN_SPEED)
         elif state == (True, True, True, False, False):
-            print("left crazy")
+            # print("left crazy")
             action = (-1 * TURN_SPEED, 1.2 * TURN_SPEED)
-        else:
-            print(f"WTF prev state {prev_state}")
+        # else:
+            # print(f"WTF prev state {prev_state}")
 
-        prev_state = state
         yield action
 
 
